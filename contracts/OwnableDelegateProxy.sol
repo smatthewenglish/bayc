b20f137c5bf84eab607b7e71139e0643a2cb793d
smatthewenglish <s.matthew.english@gmail.com>
2021-07-09 21:28:26 -0400

    Partition

---
+++ contracts/OwnableDelegateProxy.sol
@@ -0,0 +1,11 @@
+contract OwnableDelegateProxy is OwnedUpgradeabilityProxy {
+
+    constructor(address owner, address initialImplementation, bytes calldata)
+        public
+    {
+        setUpgradeabilityOwner(owner);
+        _upgradeTo(initialImplementation);
+        require(initialImplementation.delegatecall(calldata));
+    }
+
+}
---
+++ contracts/ReentrancyGuarded.sol
@@ -0,0 +1,15 @@
+contract ReentrancyGuarded {
+
+    bool reentrancyLock = false;
+
+    /* Prevent a contract function from being reentrant-called. */
+    modifier reentrancyGuard {
+        if (reentrancyLock) {
+            revert();
+        }
+        reentrancyLock = true;
+        _;
+        reentrancyLock = false;
+    }
+
+}
