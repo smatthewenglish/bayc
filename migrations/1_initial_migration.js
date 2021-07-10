b95f848831dc59812638a9b7cbd14b15e601452e
smatthewenglish <s.matthew.english@gmail.com>
2021-07-09 21:46:40 -0400

    Deployable, bugs to resolve

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,23 @@
-const Migrations = artifacts.require("Migrations");
+/**
+ * @dev Deployment configuration.
+ */
+
+// const BuilderShop = artifacts.require("BuilderShop");
+
+// let niftyRegistryContract = "0xCefBf44ff649B6E0Bc63785699c6F1690b8cF73b"; //Rinkeby
+
+// let defaultOwner = "0xBDbAEe6326cF7164EDaf107C525c1928B66d133f";
+
+// module.exports = function(deployer) {
+// 	deployer.deploy(
+// 		BuilderShop,
+// 		niftyRegistryContract,
+// 		defaultOwner
+// 	);
+// };
+
+const WyvernExchange = artifacts.require("WyvernExchange");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(WyvernExchange);
 };
